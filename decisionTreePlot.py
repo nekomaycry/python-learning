4d65ebb18565c4fccee4c8c5324e6c8a622f811a
nekomaycry <nekomaycry@gmail.com>
2018-04-13 15:18:27 +0800

    c

---
+++ decisionTreePlot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle='sawtooth',fs='0.8')
+leafNode = dict(boxstyle='round4',fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+
+def getNumLeafs(myTree):
+    pass
+def getTreeDepth(myTree):
+    pass
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    pass
+def plotMidText(cntrPt, parentPt, txtString):
+    pass
+def plotTree(myTree, parentPt, nodeTxt):
+    pass
+def createPlot(inTree):
+    pass
+
